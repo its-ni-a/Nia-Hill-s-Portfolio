4727df7594c1347470bf5838d641fd3701b65286
Nia Hill <ndeborahill@gmail.com>
2022-07-28 01:08:55 -0400

    new navbar and other adjustments

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link')
+
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
+
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+    const visibility = primaryNav.getAttribute("data-visible")
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute('aria-expanded', true)
+    }   else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute("aria-expanded", false)
+    }
+});
